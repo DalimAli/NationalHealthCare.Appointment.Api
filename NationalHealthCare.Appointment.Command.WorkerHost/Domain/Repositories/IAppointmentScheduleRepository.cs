a1c3b5056e5f1f659fa84be6aede558d4af50940
Rafat Ahmad <rafat-ahmad@LIVE.COM>
2025-12-27 15:34:02 +0600

    Added domains and aggregates

---
+++ NationalHealthCare.Appointment.Command.WorkerHost/Domain/Repositories/IAppointmentScheduleRepository.cs
@@ -0,0 +1,13 @@
+using NationalHealthCare.Appointment.Command.WorkerHost.Domain.Aggregates;
+
+namespace NationalHealthCare.Appointment.Command.WorkerHost.Domain.Repositories;
+
+public interface IAppointmentScheduleRepository
+{
+    Task<AppointmentSchedule> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+    Task<AppointmentSchedule> GetByDateAsync(DateTime scheduleDate, CancellationToken cancellationToken = default);
+    Task<IEnumerable<AppointmentSchedule>> GetAllAsync(CancellationToken cancellationToken = default);
+    Task AddAsync(AppointmentSchedule schedule, CancellationToken cancellationToken = default);
+    Task UpdateAsync(AppointmentSchedule schedule, CancellationToken cancellationToken = default);
+    Task DeleteAsync(Guid id, CancellationToken cancellationToken = default);
+}
