0a213705b85c6c045cbc82a6566f216c5d24a68a
DalimAli <dalim3281@gmail.com>
2025-12-27 11:44:07 +0600

    end-point ready

--- NationalHealthCare.Appointment.Api/Controllers/AppointmentController.cs
+++ NationalHealthCare.Appointment.Api/Controllers/AppointmentController.cs
@@ -1,17 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using NationalHealthCare.Appointment.Api.Models;
+using NationalHealthCare.Appointment.SharedKarnel;
+using SharedKernel.Abstractions;
 
 namespace NationalHealthCare.Appointment.Api.Controllers
 {
     [Route("api/appointment")]
     [ApiController]
-    public class AppointmentController : ControllerBase
+    public class AppointmentController(IServiceBus serviceBus) : ControllerBase
     {
 
         [HttpPost]
         [Route("schedule")]
-        public IActionResult ScheduleAppointment([FromBody] AppointmentRequest request)
+        public async Task<IActionResult> ScheduleAppointment([FromBody] AppointmentRequest appointmentRequest)
         {
+            var request = new CreateAppointmentCommand
+            {
+                PatientName = appointmentRequest.PatientName,
+                AppointmentDate = appointmentRequest.AppointmentDate,
+                DoctorName = appointmentRequest.DoctorName,
+                ReasonForVisit = appointmentRequest.ReasonForVisit
+            };
+
+            await serviceBus.PublishAsync(request);
             return Ok(new { Message = "Appointment scheduled successfully", AppointmentDetails = request });
         }
     }
