0884746d78e15df455da8c67c0f0889607b019fb
DalimAli <dalim3281@gmail.com>
2025-12-27 11:04:01 +0600

    appintment controller created

---
+++ NationalHealthCare.Appointment.Api/Controllers/AppointmentController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NationalHealthCare.Appointment.Api.Models;
+
+namespace NationalHealthCare.Appointment.Api.Controllers
+{
+    [Route("api/appointment")]
+    [ApiController]
+    public class AppointmentController : ControllerBase
+    {
+
+        [HttpPost]
+        [Route("schedule")]
+        public IActionResult ScheduleAppointment([FromBody] AppointmentRequest request)
+        {
+            return Ok(new { Message = "Appointment scheduled successfully", AppointmentDetails = request });
+        }
+    }
+}
---
+++ NationalHealthCare.Appointment.Api/Models/AppointmentRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace NationalHealthCare.Appointment.Api.Models;
+
+public class AppointmentRequest
+{
+    public string PatientName { get; set; }
+    public DateTime AppointmentDate { get; set; }
+    public string DoctorName { get; set; }
+    public string ReasonForVisit { get; set; }
+}
